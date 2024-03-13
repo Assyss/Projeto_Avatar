ef8342a3fb07f22be0f329f7bc8f3386d6a45ac9
Henrique <henrique.scastro12@gmail.com>
2024-03-13 19:04:37 -0300

    aba de cursos feita (e outras mudanças)

--- routes/quest.ts
+++ routes/quest.ts
@@ -1,7 +1,9 @@
 import app = require("teem");
 
 class AvatarRoute{
-    
+    public async cursos(req: app.Request, res: app.Response) {
+		res.render("index/cursos");
+	};
 }
 
 export = AvatarRoute;
