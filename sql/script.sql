bbd02388eba782972ea1b57eed6fa1ca59ee95d7
Antonio-czr29 <130580303+tonhao29@users.noreply.github.com>
2024-04-29 10:37:53 -0300

    banco
    .

--- sql/script.sql
+++ sql/script.sql
@@ -26,14 +26,6 @@ CREATE TABLE pergunta (
   id_pergunta INT NOT NULL,
   descricao VARCHAR(500) NOT NULL,
   id_curso INT NOT NULL,
-  PRIMARY KEY (id_pergunta),
-  INDEX fk_pergunta_curso1_idx (id_curso ASC) VISIBLE,
-  CONSTRAINT fk_pergunta_curso1 FOREIGN KEY (id_curso) REFERENCES curso (id_curso) ON DELETE NO ACTION ON UPDATE NO ACTION
-);
-
-CREATE TABLE alternativa (
-  id_alternativa INT NOT NULL,
-  descricao VARCHAR(500) NOT NULL,
   ponto0 INT NOT NULL,
   ponto1 INT NOT NULL,
   ponto2 INT NOT NULL,
@@ -42,10 +34,9 @@ CREATE TABLE alternativa (
   ponto5 INT NOT NULL,
   ponto6 INT NOT NULL,
   ponto7 INT NOT NULL,
-  id_pergunta INT NOT NULL,
-  PRIMARY KEY (id_alternativa),
-  INDEX fk_alternativa_pergunta1_idx (id_pergunta ASC) VISIBLE,
-  CONSTRAINT fk_alternativa_pergunta1 FOREIGN KEY (id_pergunta) REFERENCES pergunta (id_pergunta) ON DELETE NO ACTION ON UPDATE NO ACTION
+  PRIMARY KEY (id_pergunta),
+  INDEX fk_pergunta_curso1_idx (id_curso ASC) VISIBLE,
+  CONSTRAINT fk_pergunta_curso1 FOREIGN KEY (id_curso) REFERENCES curso (id_curso) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
 
 CREATE TABLE resultado_final (
@@ -68,13 +59,11 @@ CREATE TABLE resultado_final (
 CREATE TABLE resposta (
   id_resposta INT NOT NULL AUTO_INCREMENT,
   id_pergunta INT NOT NULL,
-  id_alternativa INT NOT NULL,
   id_resultado_final INT NOT NULL,
+  valor INT NOT NULL,
   PRIMARY KEY (id_resposta),
   INDEX fk_resposta_pergunta1_idx (id_pergunta ASC) VISIBLE,
-  INDEX fk_resposta_alternativa1_idx (id_alternativa ASC) VISIBLE,
   INDEX fk_resposta_resultado_final1_idx (id_resultado_final ASC) VISIBLE,
   CONSTRAINT fk_resposta_pergunta1 FOREIGN KEY (id_pergunta) REFERENCES pergunta (id_pergunta) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT fk_resposta_alternativa1 FOREIGN KEY (id_alternativa) REFERENCES alternativa (id_alternativa) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT fk_resposta_resultado_final1 FOREIGN KEY (id_resultado_final) REFERENCES resultado_final (id_resultado_final) ON DELETE NO ACTION ON UPDATE NO ACTION
 );
